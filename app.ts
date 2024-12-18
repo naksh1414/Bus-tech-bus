09abc1dbba6e960ab4501a18ad1411409544d397
nakshatra <nakshatramanglik14@gmail.com>
2024-12-18 22:12:05 +0530

    Bus service

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
